af47c72fe1fe279790b9b173596ad594d7054f31
John Hess & Aaron Jacobowitz <pair+alookatommorow.amjacobowitz@devbootcamp.com>
2015-07-30 12:23:12 -0700

    add index show and new pages and other stuff

---
+++ app/controllers/task_lists_controller.rb
@@ -0,0 +1,31 @@
+class TaskListsController < ApplicationController
+
+  def index
+    @task_lists = TaskList.all
+  end
+
+  def show
+    @task_list = TaskList.find(params[:id])
+  end
+
+  def new
+    @task_list = TaskList.new()
+  end
+
+  def create
+    @task_list = TaskList.new(task_list_params)
+    if @task_list.save!
+      redirect_to @task_list
+    else
+      render 'new'
+    end
+  end
+
+
+  private
+
+  def task_list_params
+    params.require(:task_list).permit(:name)
+  end
+
+end
